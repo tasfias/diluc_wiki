813de6774b1fc8ac0bf53aca255a606fbd1489a6
tasfias <tasfiasaiyeda@gmail.com>
2026-07-14 16:17:02 +0100

    docker setup with postgresql

---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Waiting for PostgreSQL..."
+
+while ! nc -z db 5432; do
+  sleep 1
+done
+
+echo "PostgreSQL is ready!"
+
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
